4cdf51f469aa79689dd8f41125ed1d1e0bccedaf
garethconnop <g.connop@btinternet.com>
2015-03-07 22:51:46 +0000

    Add paperclip and user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,6 @@ class ApplicationController < ActionController::Base
   	devise_parameter_sanitizer.for(:account_update) << :first_name
   	devise_parameter_sanitizer.for(:account_update) << :last_name
   	devise_parameter_sanitizer.for(:account_update) << :username
+  	devise_parameter_sanitizer.for(:account_update) << :avatar
   end
 end
