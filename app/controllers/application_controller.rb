27e534b383cbcb288d27067728cca56243361ef2
Gareth Connop <g.connop@btinternet.com>
2015-03-09 19:35:04 +0000

    Add ability to login with email or username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,22 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
   private
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) << :first_name
-  	devise_parameter_sanitizer.for(:sign_up) << :last_name
   	devise_parameter_sanitizer.for(:sign_up) << :username
+
+
+
+
+    devise_parameter_sanitizer.for(:sign_in) << :login
+    devise_parameter_sanitizer.for(:sign_in) << :username
+
+
+
+
   	devise_parameter_sanitizer.for(:account_update) << :first_name
   	devise_parameter_sanitizer.for(:account_update) << :last_name
   	devise_parameter_sanitizer.for(:account_update) << :username
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,21 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
+  #Login with username or email
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  #Avatar
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
