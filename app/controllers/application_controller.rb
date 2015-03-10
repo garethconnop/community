ff8059b76bd6611030949124bf32ea4c9b4dab9f
Gareth Connop <g.connop@btinternet.com>
2015-03-10 18:12:23 +0000

    Fix for blank emailerror

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:sign_in) << :login
-    devise_parameter_sanitizer.for(:sign_in) << :username
-  	devise_parameter_sanitizer.for(:account_update) << :first_name
-  	devise_parameter_sanitizer.for(:account_update) << :last_name
-  	devise_parameter_sanitizer.for(:account_update) << :username
-  	devise_parameter_sanitizer.for(:account_update) << :avatar
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :avatar, :password, :password_confirmation, :current_password) }
   end
 end
