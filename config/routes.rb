491384010f253b573c119aa631f2f4faa627c772
Gareth Connop <g.connop@btinternet.com>
2015-03-16 09:57:07 +0000

    Add further styling and layout to forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
 
   devise_scope :user do
-  	get 'join', to: 'devise/registrations#new', as: :join
-  	get 'sign-in', to: 'devise/sessions#new', as: :sign_in
-  	get 'edit', to: 'devise/registrations#edit', as: :edit
+    get 'join', to: 'devise/registrations#new', as: :join
+    get 'sign-in', to: 'devise/sessions#new', as: :sign_in
+    delete 'sign-out' => 'devise/sessions#destroy', :as => :sign_out
   end
-  
+
   resources :posts, path: "forum" do
   	resources :comments, path: "post"
   end
